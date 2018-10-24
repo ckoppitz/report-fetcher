6ae4e63f7a5d67e9bd98a2572a2afc09fddc07eb
Conrad Koppitz <ckoppitz@salmon.com>
2018-10-24 16:20:49 -0700

    Boilerplate

---
+++ src/main/java/com/salmon/sde/reportfetcher/ReportfetcherApplication.java
@@ -0,0 +1,13 @@
+package com.salmon.sde.reportfetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportfetcherApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ReportfetcherApplication.class, args);
+	}
+}
---
+++ src/test/java/com/salmon/sde/reportfetcher/ReportfetcherApplicationTests.java
@@ -0,0 +1,16 @@
+package com.salmon.sde.reportfetcher;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReportfetcherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
